55a01c62705e65acdae0a2f6ef40560096d3ca34
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-14 11:22:31 +0600

    hooks counter app created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import Counter from './components/Counter';
+import HooksCounter from './components/HooksCounter';
 import { store } from './redux/store';
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
         </h1>
 
         <div className="max-w-md mx-auto mt-10 space-y-5">
+          {/* <Counter />
           <Counter />
-          <Counter />
-          <Counter />
+          <Counter /> */}
+          <HooksCounter />
         </div>
       </div>
     </Provider>
